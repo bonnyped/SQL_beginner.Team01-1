522489a7d34db38f29281de8d84553e2d7fe0755
bonnyped <bonnyped@student.21-school.ru>
2023-11-21 14:32:38 +0600

    minor changes

--- src/ex01/team01_ex01.sql
+++ src/ex01/team01_ex01.sql
@@ -42,8 +42,8 @@ WITH before_after_rate AS (
         INNER JOIN balance AS b ON b.currency_id = c.id
     GROUP BY b.user_id,
         c.name,
-        c.id,
         b.updated,
+        c.id,
         b.user_id,
         b.currency_id,
         b.money
@@ -59,3 +59,60 @@ FROM "user" AS u
 ORDER BY 1 DESC,
     2,
     3;
+
+WITH foo AS (SELECT b.updated        balance_update,
+                    (SELECT CASE
+                                WHEN EXISTS(SELECT b.updated
+                                            FROM currency c
+                                            WHERE c.updated < b.updated
+                                              AND c.id = b.currency_id) THEN (SELECT c2.updated
+                                                                              FROM balance b2
+                                                                                       JOIN currency c2 on b.currency_id = c2.id
+                                                                              WHERE c2.updated < b.updated
+                                                                              ORDER BY 1 DESC
+                                                                              LIMIT 1)
+                                END) currency_update,
+                    money,
+                    b.user_id
+             FROM balance b),
+     boo AS (SELECT b.updated        balance_update,
+                    (SELECT CASE
+                                WHEN NOT EXISTS(SELECT b.updated
+                                                FROM currency c
+                                                WHERE c.updated < b.updated
+                                                  AND c.id = b.currency_id) THEN (SELECT c2.updated
+                                                                                  FROM balance b2
+                                                                                           JOIN currency c2 on b.currency_id = c2.id
+                                                                                  WHERE c2.updated > b.updated
+                                                                                  ORDER BY 1
+                                                                                  LIMIT 1)
+                                END) currency_update,
+                    money,
+                    b.user_id
+             FROM balance b
+             ORDER BY 3 DESC),
+     together AS (
+         SELECT *
+         FROM foo
+         WHERE currency_update IS NOT NULL
+         UNION
+         SELECT *
+         FROM boo
+         WHERE currency_update IS NOT NULL),
+     just_little_bit AS (SELECT COALESCE(u.name, 'not defined') name,
+                                         COALESCE(u.lastname, 'not defined') lastname,
+                                         c3.name currency_name,
+                                         t.*
+                         FROM "user" u
+                                  FULL JOIN balance b3 on u.id = b3.user_id
+                                  FULL JOIN currency c3 on b3.currency_id = c3.id
+                                  FULL JOIN together t
+                                       ON t.balance_update = b3.updated AND t.currency_update = c3.updated AND
+                                          t.money = b3.money AND b3.user_id = t.user_id)
+SELECT jlb.name,
+       jlb.lastname,
+       jlb.currency_name,
+       jlb.money * c4.rate_to_usd currency_in_usd
+FROM just_little_bit jlb
+         JOIN currency c4 ON c4.updated = jlb.currency_update AND c4.name = jlb.currency_name
+ORDER BY 1 DESC, 2, 3;
